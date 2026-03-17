b30a7874243b658353ea466480e7a00cfb33ca8c
scott <scott.galloway@gmail.com>
2026-03-17 00:03:45 +0000

    feat: add CompanyType enum to Core

---
+++ src/lucidRESUME.Core/Models/Jobs/CompanyType.cs
@@ -0,0 +1,14 @@
+namespace lucidRESUME.Core.Models.Jobs;
+
+public enum CompanyType
+{
+    Unknown,
+    Startup,
+    ScaleUp,
+    Enterprise,
+    Agency,
+    Consultancy,
+    Finance,
+    Public,      // gov / NHS / charity / non-profit
+    Academic
+}
